18ad40b38195e4615817da64d21dcbb5f4781e59
Sebastian Rojas <88469107+Petulio14@users.noreply.github.com>
2022-09-13 23:07:30 -0500

    Update Ingrediente.cs

---
+++ taller2/entregable2p2/Ingrediente.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+namespace entregable2p2
+{
+   public class Ingredientes{
+    
+   public string nombres {get; set ;}
+   public  int cantidad {get; set ;}
+   public double precio {get; set ;}
+
+   public Ingredientes(string nombres, int cantidad, double precio){
+        this.nombres=nombres;
+        this.cantidad=cantidad;
+        this.precio=precio;
+     }
+    }
+ }
